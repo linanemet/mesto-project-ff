01992209f24aacaa7558300c0f7881775cc39330
Polina Kuznetsova <metlyakovaps@yandex.ru>
2025-04-24 13:38:35 +0500

    version 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(cardData, cardDeleteCallback) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
 
-// @todo: Функция создания карточки
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
 
-// @todo: Функция удаления карточки
+  cardDeleteButton.addEventListener("click", () =>
+    cardDeleteCallback(cardElement)
+  );
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+function deleteCard(currentCard) {
+  currentCard.remove();
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, deleteCard);
+  cardList.append(cardElement);
+});
